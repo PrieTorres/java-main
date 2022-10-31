179e74b978bde519d8155d5261f949cdcc187ce3
PRISCILA TORRES BENEDITO DE PAULA <priscila_paula@estudante.sc.senai.br>
2022-10-31 09:43:27 -0300

    up

---
+++ WallpaperNext/src/objetos/Foto.java
@@ -0,0 +1,15 @@
+package objetos;
+
+public class Foto {
+
+	public String link;
+	public String descricao;
+	
+	public Foto(String link, String descricao) {
+		super();
+		this.link = link;
+		this.descricao = descricao;
+	}
+	
+
+}
--- WallpaperNext/src/objetos/pasta.java
+++ WallpaperNext/src/objetos/pasta.java
@@ -1,5 +1,19 @@
 package objetos;
 
 public class pasta {
+	
+	Foto[] fotos = new Foto[100];
+	public String nome;
+	static public int quantidadePastas=0;
+	public String descricao;
+	
+	public pasta(String nome, String descricao) {
+		super();
+		this.nome = nome;
+		this.descricao = descricao;
+		this.quantidadePastas++;
+	}
+	
+	
 
 }
